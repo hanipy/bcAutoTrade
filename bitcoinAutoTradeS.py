464b2917946a0d267deb2ec458d7fb4787eec062
hanipy <90880083+hanipy@users.noreply.github.com>
2021-09-29 12:37:07 +0900

    Add files via upload

--- bitcoinAutoTradeS.py
+++ bitcoinAutoTradeS.py
@@ -16,19 +16,19 @@ def post_message(token, channel, text):
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_ma15(ticker):
     """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=15)
     ma15 = df['close'].rolling(15).mean().iloc[-1]
     return ma15
 
@@ -56,21 +56,21 @@ post_message(myToken,"#coin", "autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XTZ")
+        start_time = get_start_time("KRW-XRP")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XTZ", 0.2)
-            ma15 = get_ma15("KRW-XTZ")
-            current_price = get_current_price("KRW-XTZ")
+            target_price = get_target_price("KRW-XRP", 0.7)
+            ma15 = get_ma15("KRW-XRP")
+            current_price = get_current_price("KRW-XRP")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-XTZ", krw*0.9995)
+                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
         else:
-            btc = get_balance("XTZ")
+            btc = get_balance("XRP")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-XTZ", btc*0.9995)
+                upbit.sell_market_order("KRW-XRP", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
